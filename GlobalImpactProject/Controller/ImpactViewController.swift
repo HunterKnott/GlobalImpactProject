dc6e3a3b8da4836a4a416aa50d5be34aeafb6c63
HunterKnott <hunterknott00@gmail.com>
2019-01-10 12:27:58 -0700

    Initial Commit

---
+++ GlobalImpactProject/Controller/ImpactViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ImpactViewController.swift
+//  GlobalImpactProject
+//
+//  Created by Knott, Hunter on 1/10/19.
+//  Copyright © 2019 Knott, Hunter. All rights reserved.
+//
+
+import UIKit
+
+class ImpactViewController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
